93714a0d6e9d2e3b43125554bfe3da4a8b80b4cb
StasyaV <stasya522@mail.ru>
2020-09-29 23:33:34 +0300

    Стилизация мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -2,11 +2,11 @@
 
 window.addEventListener("load", function () {
   var menu = document.querySelector('.header__navigation');
-  var menuToggle = document.querySelector('.menu-button');
+  var menuToggle = document.querySelector('.header__menu-button');
 
   if (menuToggle) {
     menuToggle.addEventListener('click', function () {
-      menuToggle.classList.toggle('menu-button--closed');
+      menuToggle.classList.toggle('header__menu-button--closed');
       menu.classList.toggle('header__navigation--close');
     });
   }
